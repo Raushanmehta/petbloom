c605edbd5b5bc79d1018899a44f9e9b4afa7e6f2
Raushanmehta <raushanmehta2184@gmail.com>
2026-09-09 18:04:10 +0530

    done

--- sections/home/HeroSection.tsx
+++ sections/home/HeroSection.tsx
@@ -52,7 +52,7 @@ export default function HeroSection() {
                         return (
                             <CarouselItem
                                 key={slide.id}
-                                className="relative flex min-h-[610px] h-[calc(100vh-2rem)] sm:h-[calc(100vh-2rem)] md:h-[calc(100vh-3rem)] lg:h-[calc(100vh-4rem)] items-center pl-0 rounded-[2.5rem] sm:rounded-[2.5rem]"
+                                className="relative flex min-h-[610px] h-[calc(100vh-2rem)] sm:h-[calc(100vh-2rem)] md:h-[calc(100vh-3rem)] lg:h-[calc(100vh-3rem)] items-center pl-0 rounded-[2.5rem] sm:rounded-[2.5rem]"
                             >
                                 {/* Background Image */}
                                 <div className="absolute inset-0 z-0 h-full w-full overflow-hidden rounded-2xl sm:rounded-[2.5rem]">
